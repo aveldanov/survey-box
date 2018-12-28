ecd3fc88e92604486d17e7f4cd8d6df9320160db
Veldanov <aveldanov@MTVL16074381b.local>
2018-12-28 15:24:07 -0800

    updated

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 
 const app = express();
 
+// initiate passport auth (two args)
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile)
+  }));
 
-//client ID   909857137406-8pl28scfsjnnrkrqghuhg6u24ur2oo4h.apps.googleusercontent.com
+//route handler (middleware)
+// 'google' --> Google Strategy identifier
+// level of access to user's profile
 
-//client Secret   2lhsmWf_RcaLXrSB2Lgs0saP
-passport.use(new GoogleStrategy());
+app.get('/auth/google', passport.authenticate('google',
+  {
+    scope: ['profile', 'email']
+  }
+)
+);
 
-
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 4000;
