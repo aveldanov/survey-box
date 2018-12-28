0b7f1ed7c544e8835a9dffb07055876262a88b29
Veldanov <aveldanov@MTVL16074381b.local>
2018-12-28 13:32:07 -0800

    updated

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
 
+//client ID   909857137406-8pl28scfsjnnrkrqghuhg6u24ur2oo4h.apps.googleusercontent.com
 
-app.get('/', (req, res) => {
-  res.send({
-    bye: 'buddy'
-  })
-});
+//client Secret   2lhsmWf_RcaLXrSB2Lgs0saP
+passport.use(new GoogleStrategy());
 
 
 
