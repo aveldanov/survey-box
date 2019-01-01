0e74d5a32876a448877665b84e0af0ee8a3c43c8
Veldanov <aveldanov@MTVL16074381b.local>
2018-12-31 21:46:44 -0800

    updated

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+        <a href="/auth/google">Sign In with Google</a>
+      </div>
+    );
+  }
+}
+
+export default App;
