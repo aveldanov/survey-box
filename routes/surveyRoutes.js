e6ad6c70c568fe147d96bf6f327a4255d00f3beb
Veldanov <anton.veldanov@gmail.com>
2019-01-21 19:37:06 -0800

    local tunnel

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,13 +4,36 @@ const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const Survey = mongoose.model('surveys');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
-
-
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 
 module.exports = app => {
 
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thank you for your feedback');
+  });
+
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    // console.log(req.body);
+    //res.send({});
+    const events = _.map(req.body, ({ email, url }) => {
+
+      const pathname = new URL(url).pathname;
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      const match = p.test(pathname);
+      if (match) {
+        return {
+          email: email,
+          surveyId: match.surveyId,
+          choice: match.choice
+        };
+      }
+    })
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    console.log(uniqueEvents)
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
